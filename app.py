255d76b7ba314614612ed2176c80c790ceb2f666
Habibu Armayau <habibuarmayau@gmail.com>
2025-09-21 15:12:40 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body').lower()
+    response = MessagingResponse()
+
+    if 'sallama' in msg:
+        response.message("Taya zan taimaka maka?")
+    elif 'nhis' in msg:
+        response.message("NHIS a AKTH yana bada sabis ga ma’aikata da iyalansu.")
+    else:
+        response.message("Na fahimci tambayarka ba sosai ba. Zan turawa likita.")
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
